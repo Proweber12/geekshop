ecc0bdd575e306a9eba59c6f8789d825826049f9
Mihail <borisonchick@gmail.com>
2022-01-17 14:53:38 +0300

    Lesson 5. Code style fix

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
+from django.shortcuts import HttpResponseRedirect, get_object_or_404
 
 from basketapp.models import Basket
 from mainapp.models import Product
