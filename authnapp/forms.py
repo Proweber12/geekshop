6e7ad20dff80675469682dd86131ba6925e3298b
Mihail <borisonchick@gmail.com>
2022-01-28 18:17:05 +0300

    lesson_9. Send email to file.

--- authnapp/forms.py
+++ authnapp/forms.py
@@ -1,3 +1,6 @@
+import hashlib
+import random
+
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
 
@@ -33,6 +36,16 @@ class ShopUserRegisterForm(UserCreationForm):
         if data[0] == data[0].lower():
             raise forms.ValidationError("Имя написано в неправильном регистре!")
         return data
+
+    def save(self):
+        user = super(ShopUserRegisterForm, self).save()
+
+        user.is_active = False
+        salt = hashlib.sha1(str(random.random()).encode("utf8")).hexdigest()[:6]
+        user.activation_key = hashlib.sha1((user.email + salt).encode("utf8")).hexdigest()
+        user.save()
+
+        return user
 
     class Meta:
         model = ShopUser
