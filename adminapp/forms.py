811fb2abca70a57e898791413d041fe161a5131a
Mihail <borisonchick@gmail.com>
2022-02-21 15:23:02 +0300

    lesson_15. Changes from lesson_14 and code style fix

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -8,7 +8,21 @@ from mainapp.models import Product, ProductCategory
 class ShopUserAdminEditForm(ShopUserEditForm):
     class Meta:
         model = ShopUser
-        fields = "__all__"
+        fields = (
+            "avatar",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "age",
+            "activation_key",
+            "activation_key_expires",
+            "last_login",
+            "date_joined",
+            "is_superuser",
+            "is_staff",
+            "is_active",
+        )
 
 
 class ProductCategoryEditForm(forms.ModelForm):
